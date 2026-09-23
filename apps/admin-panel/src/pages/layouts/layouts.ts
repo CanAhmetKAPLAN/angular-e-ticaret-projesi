d101e5de112fa757beabfa80f6c8603d487b84eb
Can Ahmet KAPLAN <cakaplan06@gmail.com>
2026-09-23 20:40:09 +0300

    footer düzenlendi

--- apps/admin-panel/src/pages/layouts/layouts.ts
+++ apps/admin-panel/src/pages/layouts/layouts.ts
@@ -4,13 +4,28 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { navigations } from '../../navigation';
 import { NavPipe } from '../../pipes/nav-pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  imports: [Breadcrumb, RouterLink, RouterLinkActive, NavPipe, FormsModule],
+  imports: [
+    Breadcrumb,
+    RouterLink,
+    RouterLinkActive,
+    NavPipe,
+    FormsModule,
+    DatePipe,
+  ],
   templateUrl: './layouts.html',
 })
 export default class Layouts {
   readonly search = signal<string>('');
+  readonly time = signal<Date | string>(new Date());
   readonly navigations = computed(() => navigations);
+
+  constructor() {
+    setInterval(() => {
+      this.time.set(new Date());
+    }, 1000);
+  }
 }
